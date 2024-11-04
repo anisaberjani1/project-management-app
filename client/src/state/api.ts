5916054abf5cc96a26e8aa720b883479a5c010fd
anisaberjani1 <anisa.berjani@gmail.com>
2024-11-04 18:17:40 +0100

    api endpoints

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -60,6 +60,7 @@ export interface Task{
     attachments?: Attachment[];
 }
 
+//api endpoints
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: 'api',
