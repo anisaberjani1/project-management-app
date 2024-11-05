073b5f0fe019200a22ea96a3178533ca406264bf
anisaberjani1 <anisa.berjani@gmail.com>
2024-11-05 15:22:08 +0100

    ListView Page

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import ProjectHeader from '@/app/projects/ProjectHeader';
 import Board from '../BoardView';
+import List from '../ListView';
 
 type Props = {
     params: {id:string}
@@ -21,6 +22,9 @@ const Project = ({params}: Props) => {
         
         {activeTab === "Board" && (
             <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
+        )}
+        {activeTab === "List" && (
+            <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
         )}
     </div>
   )
