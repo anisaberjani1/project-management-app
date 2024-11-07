e49c78539639b6478bb2d86c7fc9b1b56640eca2
anisaberjani1 <anisa.berjani@gmail.com>
2024-11-07 15:57:22 +0100

    Timeline Page

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Header = ({name, buttonComponent, isSmallText = false}: Props) => {
   return (
     <div className='mb-5 flex w-full items-center justify-between'>
-        <h1 className={`${isSmallText ? "lext-lg" : "text-2xl"} font-semibolddark:text-white`}>
+        <h1 className={`${isSmallText ? "lext-lg" : "text-2xl"} font-semibold dark:text-white`}>
             {name}
         </h1>
         {buttonComponent}
