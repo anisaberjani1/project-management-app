ca555d39b70067d59829482a4a42e90320d81cf5
anisaberjani1 <anisa.berjani@gmail.com>
2024-11-08 21:09:51 +0100

    Home Page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,8 @@
+import HomePage from "./home/page";
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        hi
-      </main>
+      <HomePage/>
   );
 }
 
