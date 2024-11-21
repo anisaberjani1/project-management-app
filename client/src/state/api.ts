586b9859eb6a140a118a662f857853d803f6f898
anisaberjani1 <anisa.berjani@gmail.com>
2024-11-21 01:21:25 +0100

    small modifications

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -85,31 +85,29 @@ export const api = createApi({
                 headers.set("Authorization", `Bearer ${accessToken}`);
             }
             return headers;
-        },
+        }
     }),
     reducerPath: 'api',
     tagTypes: ["Projects", "Tasks", "Users", "Teams"],
     endpoints: (build) => ({
         getAuthUser: build.query({
-            queryFn: async (_,_queryApi, _extraoptions, fetchWithBQ) => {
-                try{
-                    // grab the info from cognito
-                    const user = await getCurrentUser();
-                    const session = await fetchAuthSession();
-                    if(!session) throw new Error("No session found");
-                    const { userSub } = session;
-                    const { accessToken } = session.tokens ?? {};
-
-                    //api request from our own databases
-                    const userDetailsResponse = await fetchWithBQ(`users/${userSub}`);
-                    const userDetails = userDetailsResponse.data as User;
-
-                    return {data: {user, userSub, userDetails}}
-                }catch(error: any){
-                    return { error: error.message || "Could not fetch user data"};
-                }
-            }
-        }), 
+            queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+              try {
+                const user = await getCurrentUser();
+                const session = await fetchAuthSession();
+                if (!session) throw new Error("No session found");
+                const { userSub } = session;
+                const { accessToken } = session.tokens ?? {};
+      
+                const userDetailsResponse = await fetchWithBQ(`users/${userSub}`);
+                const userDetails = userDetailsResponse.data as User;
+      
+                return { data: { user, userSub, userDetails } };
+              } catch (error: any) {
+                return { error: error.message || "Could not fetch user data" };
+              }
+            },
+        }),
         getProjects: build.query<Project[], void>({
             query: () => 'projects',
             providesTags: ["Projects"],
